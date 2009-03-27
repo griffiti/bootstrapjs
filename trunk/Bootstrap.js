41fadc1d83b7120f614858e7b8ed39b5117eba88
griffiti93@yahoo.com <griffiti93@yahoo.com@707bc49a-c817-11dd-9042-6d746b85d38b>
2009-03-27 03:02:09 +0000

    Extracted copyright to JSBuilder.

--- trunk/Bootstrap.js
+++ trunk/Bootstrap.js
@@ -1,14 +1,3 @@
-/*
- * BootstrapJS 1.0
- * Copyright(c) 2006-2009, Jonathan Griffin
- * griffiti93@yahoo.com
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-
 /**
  * Bootstrap is an immediate self-invoked singleton class instance providing application startup
  * features, such as dynamic loading of CSS and JavaScript files. Boot sequence support, loaded script
